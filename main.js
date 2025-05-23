4fe6b6c33f03f89b8fe85078e8d766367961c22b
Maksim <maxim_sidorov37@vk.com>
2025-05-23 21:16:47 +0300

    less 3 End

---
+++ main.js
@@ -0,0 +1,44 @@
+// document.getElementById("one").innerHTML = "Maksim"
+
+// document.getElementById("one").innerHTML = 1666
+
+// document.getElementById("one").innerHTML = "<h1>anime</h1>"
+
+
+// document.querySelector("div").innerHTML = true 
+
+// // .one class 
+// // #one id
+
+// let btn = document.querySelector(".chek-in")
+// let inputIN = document.querySelector( ".text")
+// let div = document.querySelector(".out")
+
+// btn.onclick = function () {
+//     console.log("DA")
+//     console.log(inputIN.value)
+//     let e = inputIN.value
+    
+
+//     div.innerHTML = e
+// }
+
+// let myHappy = prompt("Когда  у тебя др бро ?")
+
+// if (myHappy == 22) { // True false
+//     // alert("Yes")
+//     // alert("Yes")
+//     console.log("Da")
+// } else if ( myHappy < 22 ){ //false
+//     console.log("Меньше чем нужно")
+// } else {
+//     console.log("No")
+// }
+
+let myNumber = prompt("Мое число 100") 
+
+if (myNumber > 100) {
+    alert("Yes") // True false
+} else if (myNumber < 100 ) { // false
+    console.log( "Меньше чем нужно")
+}
