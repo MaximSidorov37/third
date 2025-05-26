e04f0c8197b955a12e8d4971fa0216cb3c86eb97
Maksim <maxim_sidorov37@vk.com>
2025-05-26 20:30:00 +0300

    less 4 End

--- main.js
+++ main.js
@@ -1,44 +1,18 @@
-// document.getElementById("one").innerHTML = "Maksim"
+let sum  = prompt( "a+b") 
 
-// document.getElementById("one").innerHTML = 1666
+if ( sum > 15) {
+    console.log ("ДА")
+} else if ( sum <= 15) {
+    console.log ("НЕТ")
+} 
 
-// document.getElementById("one").innerHTML = "<h1>anime</h1>"
-
-
-// document.querySelector("div").innerHTML = true 
-
-// // .one class 
-// // #one id
-
-// let btn = document.querySelector(".chek-in")
-// let inputIN = document.querySelector( ".text")
-// let div = document.querySelector(".out")
-
-// btn.onclick = function () {
-//     console.log("DA")
-//     console.log(inputIN.value)
-//     let e = inputIN.value
     
+let a = ""
+let b = ""
 
-//     div.innerHTML = e
-// }
+console.log( a === b )
 
-// let myHappy = prompt("Когда  у тебя др бро ?")
+let g = 3
+let i = true
 
-// if (myHappy == 22) { // True false
-//     // alert("Yes")
-//     // alert("Yes")
-//     console.log("Da")
-// } else if ( myHappy < 22 ){ //false
-//     console.log("Меньше чем нужно")
-// } else {
-//     console.log("No")
-// }
-
-let myNumber = prompt("Мое число 100") 
-
-if (myNumber > 100) {
-    alert("Yes") // True false
-} else if (myNumber < 100 ) { // false
-    console.log( "Меньше чем нужно")
-}
+console.log ( g === i)
